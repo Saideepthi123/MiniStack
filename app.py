a99d38a6fe848704cde983a7d1b723745189f6ec
Saideepthi123 <saideepthi.k18@iiits.in>
2020-12-11 19:25:53 +0530

    Task-2 added

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from multiprocessing import Pool, Manager
 from datetime import datetime
-
+import operator
+import sys
 ###################################################
 
 total_df = pd.read_pickle("Preprocessed_questions_text.pkl")
@@ -91,7 +92,7 @@ def decontracted(phrase):
 	
 from nltk.corpus import stopwords
 stop_words = set(stopwords.words('english'))
-print ('list of stop words:', stop_words)
+#print ('list of stop words:', stop_words)
 
 def nlp_preprocessing(total_text):
     if type(total_text) is not int:
@@ -118,14 +119,15 @@ def cleanpunc(sentence):
 ###################################################
 
 
+
 @app.route('/')
 def hello_world():
     return flask.render_template('index.html')
 
 
-@app.route('/index')
+@app.route('/crawler')
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template('crawler.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -171,13 +173,73 @@ def predict():
     ################################################################################################
     fw = open('SimilarPosts.txt', 'w')
     for index in top_items:
-        print (total_df.iloc[index,3])
+        #print (total_df.iloc[index,3])
         fw.write(total_df.iloc[index,3] + "\n")
-        print("*************************************************************************************************************")
+        #print("*************************************************************************************************************")
     fw.close()
 
     with open('SimilarPosts.txt', 'r') as f: 
 	    return flask.render_template('similar.html', text=f.read()) 
+
+
+Tag_Rank = {}
+
+def tag_crawler(url):
+    source_code = requests.get(url).text
+    soup = BeautifulSoup(source_code, 'html.parser')
+    for tag_div in soup.find_all('div', {'class': 'post-taglist'}):
+        for tag_link in tag_div.find_all('a'):
+            tag = tag_link.string
+            if tag in Tag_Rank:
+                Tag_Rank[tag] += 1
+            else:
+                Tag_Rank[tag] = 1
+
+def ques_links_crawler(base_url, end_url, page_limit):
+    page_no = 1
+    while page_no <= page_limit:
+        page_url = base_url + str(page_no) + end_url
+        source_code = requests.get(page_url).text
+        soup = BeautifulSoup(source_code, 'html.parser')
+        if page_no is 1:
+            os.system('clear')
+        print('crawling page ' + str(page_no) + ': [', end='')
+        prev_len = 0
+        q_no = 1
+        for ques_link in soup.find_all('a', {'class': 'question-hyperlink'}):
+            url = 'http://stackoverflow.com/' + ques_link.get('href')
+            tag_crawler(url)
+            for _ in range(prev_len):
+                print('\b', end='')
+            print('#', end='')
+            p_cent = q_no*2
+            percent = '] (' + str(p_cent) + '%)'
+            prev_len = len(percent)
+            print(percent, end='')
+            sys.stdout.flush()
+            q_no += 1
+        page_no += 1
+
+
+@app.route('/popular', methods=['POST'])
+def pop():
+
+    page = request.form.to_dict()
+    print(type(page['pop_text']))
+    print(page['pop_text'])
+    page_limit = int(page['pop_text'])
+    print('starting crawling...')
+    ques_links_crawler('http://stackoverflow.com/questions?page=', '&sort=newest', page_limit)
+    fw = open('Tags_frequency.txt', 'w')
+    for key, value in sorted(Tag_Rank.items(), key=operator.itemgetter(1), reverse=True):
+        try:
+            fw.write(key + " : " + str(Tag_Rank[key]) + "\n")
+        except TypeError:
+            continue
+    print('\nResult saved to file Tags_frequency.txt')
+
+    with open('Tags_frequency.txt', 'r') as f: 
+	    return flask.render_template('populartechstack.html', text=f.read()) 
 
 if __name__ == '__main__':
     app.run()
